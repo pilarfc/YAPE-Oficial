b6219dbdefd8c0461a10027353bf67f1e9de0a97
pilarfc <pilar.fc@outlook.com>
2017-07-19 19:54:41 +0000

    segunda version

--- server.js
+++ server.js
@@ -20,14 +20,17 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(morgan(format));
 //app.use(express.static('src')); /*agregué esto*/
-app.use('/static', express.static(__dirname + '/src'))
+app.use('/static', express.static(__dirname + '/src')) //Si queremos usar alguna otra carpeta que no sea src debemos de agregar otro app.use con el nombre de la carpeta. Tambien podemos usar el modulo path.join: const path = join; app.use('/static, express.static(path.join(__dirname, 'node-modules')))
 
+/*app.get('/', (req, res) => { 
+    res.sendFile(__dirname + 'src/index.html');  //Válido también.
+}*/
 
 let router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({ name: 'yape-api',version: "0.0.1"});
-  res.sendFile(__dirname + 'src/index.html'); /*agregué esto*/
+  res.sendFile(__dirname + 'src/index.html');  //Aquí funciona porque retorna el api, es válido de ambas formas. 
 });
 
 app.use('/api',apiUsers(router,db));
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,27 +1,99 @@
 var cargarPagina = function () {
     
   $('.carousel.carousel-slider').carousel({fullWidth: true}); 
-   habilitandoBoton();
-    
+    habilitandoBoton();
+  $('#input-numero').keyup(habilitandoBoton);
+  $('#test5').change(habilitandoBoton);
+//  $('#boton-continuar').click(registrarNumero);
+   $('#usuario--nombre').keyup(datosUsuario);
+   $('#usuario--password').keyup(datosUsuario);
 }
  
-var validandoTelefono = function () {
-    var numeroCaracteres = $('#input-numero').val.length;
-    if (numeroCaracteres == 10) {
-		return true;
-	} else {
-		return false;
-	}
-}
 
-
+/*SEGUNDA PANTALLA*/
 var habilitandoBoton = function () {
-    if (validandoTelefono()) {
-		$('#boton-continuar').removeClass('disabled');
-	} else {
-        $('#boton-continuar').addClass('disabled');
-	}
+    var $botonContinuar = $('#boton-continuar');
+    var $numeroCaracteres = $('#input-numero').val().length;
+//    var $checkbox = $('test5');
+    
+    if($numeroCaracteres == 10 && $('#test5').is(":checked")) {
+			$botonContinuar.removeAttr("disabled");
+		} else {
+			$botonContinuar.attr("disabled", true);
+     }
 }
+
+
+//API
+
+//Parametros para hacer un request ajax 
+
+/*URL: http://localhost:3000/api/registerNumber
+Método HTTP: POST 
+Data: {"phone":"...", "terms":"..."}
+
+En postman en el body, (solo cuando es método post) utilizamos el x-www-form-urlencoded, agregamos las dos llames "phone" y "terms", en value ponemos los valores literales que son el teléfono y el terms que solo puede ser true o false, ya que es si acepta o no los términos y condiciones. Al enviarlo, nos otorga también el código que necesitamos. 
+
+shortHandMethods:
+
+$.post('http://localhost:3000/api/registerNumber', {
+    "phone": "5567890965", 
+    "terms": true
+}).then(function (response) {
+   console.log();
+}).catch(function(error){
+   console.log(error);
+})
+
+jQuery tiene su propio metodo de promesas llamado deferred y aqui usamos el deferred.then().
+
+Para la siguiente liga: 
+URL: http://localhost:3000/api/resendCode
+Método HTTP: POST 
+Data: {"phone":"..."}
+*/
+
+
+var registrarNumero = function (e) {
+    e.preventDefault(); //Evita que se recargue la página y no me retorne nada. 
+   
+    var $numeroTelefono = $('#input-numero').val();
+    
+    $.post('http://localhost:3000/api/registerNumber', {
+    "phone": $numeroTelefono, 
+    "terms": true
+    }).then(function (response) {
+       console.log();
+    }).catch(function(error){
+       console.log(error);
+    })
+}
+
+
+/*CUARTA PANTALLA*/
+
+var datosUsuarioEmail = function () {
+    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/; //Expresión regular para validar el email. 
+    
+    if (regex.test($('#usuario--email').val.trim())) { //Test es un método de las expresiones regularesque verifica si hay coincidencia en la cadena de texto. Devuelve true o false. 
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+var datosUsuario = function () {
+    var $botonCrearCuenta = $('#crear-cuenta');
+    var $nombreUsuario = $('#usuario--nombre').val().length;
+    var $usuarioPassword = $('#usuario--password').val().length; 
+    
+    if($nombreUsuario >= 2 && $usuarioPassword == 6) {
+			$botonCrearCuenta.removeClass("disabled");
+		} else {
+			$botonCrearCuenta.addClass("disabled");
+     }
+}
+
 
 
 $(document).ready(cargarPagina); 
